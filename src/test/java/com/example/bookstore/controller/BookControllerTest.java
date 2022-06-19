0674102270b8c56fc5ce482a04f759bed89209ec
mmartinslovik <martinslovik1122@gmail.com>
2022-06-19 21:04:43 +0200

    Implement basic BookControllerTest to test api calls headers

---
+++ src/test/java/com/example/bookstore/controller/BookControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.service.BookService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private BookModelAssembler bookModelAssembler;
+
+    @Test
+    public void testGetById() throws Exception {
+        Book book = new Book("1984", true, null);
+        book.setId(1L);
+
+        when(bookService.findById(1L)).thenReturn(Optional.of(book));
+
+        mockMvc.perform(get("/books/{id}", 1L).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
